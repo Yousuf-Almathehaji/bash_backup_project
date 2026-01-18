3419aaa264e67ce1439157fe6fc99eddb4292b45
Yousuf Almathehaji <intysftrain01@gmail.com>
2026-01-18 09:40:56 +0300

    the second edition of backup script you must use it with config.env file

--- script-bkup.sh
+++ script-bkup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#
+set -e
 
+source config.env
 #define the variables
-buck_file="Data" #write the path of the dirctory that you want to backup it or write $1 if want the user write the path
-time=$(date +"%Y-%m-%d_%H-%M-%S")
-targetpath=/home/ubuntu/buckup # you can change the path where you want
-file_name=bcukup-file-$time.tar.gz
-logfile=/home/ubuntu/buckup/filelogs.log #you can change log file where you want
-s3_backet="setting-buckup-for-yousuf" # write the name of your  s3 backet
-file_uploud="$targetpath/$file_name" 
+
+Timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+Backup_file="backup-$Timestamp.tar.gz"
+BACKUP_PATH="$Backup_Dir/$Backup_file"
+
+mkdir -p "$Backup_Dir"
 
 #if you let the user enter   the path using $1 remove the comment  from the following section
 #check buck_file is not empty
@@ -16,41 +16,54 @@ file_uploud="$targetpath/$file_name"
 #if [ -z "$1" ]
 #then
 #	echo "$time  -> enter the file path in the script arugument please" |tee -a "$logfile"
-#	exit 2
+#	exit 1
 #fi
+
+log() {
+    echo "$(date +"%F %T") -> $1" | tee -a "$Log_File"
+}
 
 #check the existing of Aws CLI
 if ! command -v aws &> /dev/null
 then
-	echo "$time -> You must to install the Aws CLI"| tee -a "$logfile"
-	exit 2
+	 log "AWS CLI is not installed. Please install AWS CLI and configure it."
+    exit 1
 
 fi
-#check exit status and the existing of the buckup and doing the buckup in else section.
-
-if [ $? -ne 2 ]
-then
-	if [ -f $file_name ]
-	then
-		echo "$time -> Error the buckup $file_name is already exist!" | tee -a "$logfile"
-	else
-		tar -czvf "$targetpath/$file_name" "$buck_file"
-		echo
-		echo "$time -> buckup done successfuly: $targetpath/$file_name " |tee -a "$logfile"
-		echo 
-		#making copy of the backup in aws s3 backet
-
-		aws s3 cp "$file_uploud" "s3://$s3_backet/"
-
-	fi
+# if you want the user to enter the path of the source backup directory  comment the following if statment
+# Check source directory
+if [ ! -d "$SOURCE_DIR" ]; then
+    log "Source directory does not exist: $SOURCE_DIR"
+    exit 1
 fi
-#check if the backup done successfuly to s3 backeet 
-if [ $? -eq 0 ]
+
+# Check if backup file already exists; if not, create the backup in the else section
+
+
+if [ -f "$BACKUP_PATH" ]
 then
-	echo "$time -> backup to s3 backet done successfuly :$s3_backet" |tee -a "$logfile"
+	log "Error: the backup $Backup_file already exists at $BACKUP_PATH!"
+	
 else
-	echo "$time -> backup to  s3 backet failed !"|tee -a "$logfile"
+	log "Starting backup..."
+	tar -czvf "$BACKUP_PATH" "$SOURCE_DIR"		# To allow dynamic source directory, replace $SOURCE_DIR with $1
+
+	log "Local backup created: $BACKUP_PATH"
+	
+		# Upload a copy of the backup to the AWS S3 bucket
+
+	if aws s3 cp "$BACKUP_PATH" "s3://$S3_Bucket/" 
+	then
+    	log "Backup uploaded to S3 bucket: $S3_Bucket"
+	else
+    	log "Error uploading backup to S3!"
+    	exit 1
+	fi
+
+
 fi
+
+
 
 
 
